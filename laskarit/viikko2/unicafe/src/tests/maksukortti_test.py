4f31e107bbf58d0a2b54ae6f60588514dd8b5b2a
Elisa Mero <elisa.mero@helsinki.fi>
2023-03-27 12:24:28 +0300

    vscode yms

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,33 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_lataa(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+    
+    def test_vaheneeko(self):
+        self.maksukortti.ota_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_ottaa(self):
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+        
+
+    def test_ei_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa") 
+
+    def test_rahat_riitti(self):
+        totuusarvo = self.maksukortti.ota_rahaa(200)
+
+        self.assertEqual(str(totuusarvo), "True")
+        
+
+    def test_rahat_ei_riittany(self):
+        totuusarvo = self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(str(totuusarvo), "False")

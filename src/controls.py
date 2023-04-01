c9ce9ea679889c385544fe32a810a87f21bae9d3
Elisa Mero <elisa.mero@helsinki.fi>
2023-04-01 23:12:35 +0300

    korjauksii

--- src/controls.py
+++ src/controls.py
@@ -1,9 +1,16 @@
 import pygame
-import mainplatform
+from mainplatform import PlatformJumpingGame
 #from elements import pygamepictures (?)
 
 class Movements:
     def __init__(self):
         self.x = 0
         self.y = 0
-        
+        self.rect = pygame.draw.circle(self.screen, "blue", (100, 100), 20)
+        self.draw()
+
+    def draw():
+        pygame.draw.circle(PlatformJumpingGame.self.screen, "blue", (100, 100), 20)
+
+if __name__ == "__main__":
+    Movements()
--- src/mainplatform.py
+++ src/mainplatform.py
@@ -2,20 +2,31 @@
 #import unittest?!
 #import os??
 
-
 import pygame
+#from controls import Movements
 
 class PlatformJumpingGame:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((840,780))
         self.screen.fill((173, 255, 255))
+        self.x = 50
+        self.y = 750
+        self.player = pygame.draw.circle(self.screen, "blue", (self.x, self.y), 20)
+
+        pygame.draw.circle(self.screen, (255,255,255), (100, 100), 20)
+        pygame.draw.circle(self.screen, (255,255,255), (150, 100), 20)
+        pygame.draw.circle(self.screen, (255,255,255), (125, 85), 20)
+        pygame.draw.circle(self.screen, (255,255,255), (125, 100), 20)
 
         self.loop()
 
     def loop(self):
         self.screenview()
         self.item1()
+        
+    def background(self):
+        pass
         
     
     def screenview(self):
@@ -27,82 +38,86 @@ class PlatformJumpingGame:
                 if happening.type == pygame.QUIT:
                     exit()
         
-    def item1(self):
-
-      #alustava kokeilu pallon hallinnasta ja luonnista:
-
-        x = 90
-        y = 650
- 
-        
-
-        
-
-        oikealle = False
-        vasemmalle = False
-        ylos = False
-        alas = False
-        clock = pygame.time.Clock()
-        
-        while True:
-            for tapahtuma in pygame.event.get():
-                if tapahtuma.type == pygame.KEYDOWN:
-                    if tapahtuma.key == pygame.K_LEFT:
-                        vasemmalle = True
-                    if tapahtuma.key == pygame.K_RIGHT:
-                        oikealle = True
-                    if tapahtuma.key == pygame.K_UP:
-                        ylos = True
-                    if tapahtuma.key == pygame.K_DOWN:
-                        alas = True
-                # huomaa ero: K_UP ja pygame.KEYUP !!
-        
-                if tapahtuma.type == pygame.KEYUP:
-                    if tapahtuma.key == pygame.K_LEFT:
-                        vasemmalle = False
-                    if tapahtuma.key == pygame.K_RIGHT:
-                        oikealle = False
-                    if tapahtuma.key == pygame.K_UP:
-                        ylos = False
-                    if tapahtuma.key == pygame.K_DOWN:
-                        alas = False
-        
-                if tapahtuma.type == pygame.QUIT:
-                    exit()
- 
-            if oikealle:
-                x += 2
-            if vasemmalle:
-                x -= 2
-            if ylos:
-                y -= 2
-            if alas:
-                y += 2
-
-            player = pygame.draw.circle(self.screen, "blue", (x, y), 20)
-
+    def drawing(self, coordinates):
+            pygame.display.update()
+            self.player = pygame.draw.circle(self.screen, "blue", (coordinates), 20)
 
             #self.screen.blit(player, (x,y)) wut
 
             pygame.display.update()
+            self.item1()
+
+    
+    def item1(self):
+        #Movements.draw() misk eitoimi importtaaminen, selvitä!
+        #alustava kokeilu pallon hallinnasta ja luonnista:
 
 
-            
+        clock = pygame.time.Clock()
+        pygame.display.update()
+
+        coordinates = (self.x, self.y)
+
+        left = False
+        right = False
+        up = False
+        down = False
+
+
+
+        while True:
+            for happens in pygame.event.get():
+                if happens.type == pygame.KEYDOWN:
+                    key = happens.key
+                    if key == pygame.K_LEFT:
+                        left = True
+                        self.x -= 10
+                        self.drawing(coordinates)
+                    if key == pygame.K_RIGHT:
+                        right = True
+                        self.x += 10
+                        self.drawing(coordinates)
+
+
+                # onko välttämättömiä hyppäämisen kannalta?:
+                    if key == pygame.K_UP:
+                        up = True
+                        self.y -= 10
+                        self.drawing(coordinates)
+                    if key == pygame.K_DOWN:
+                        down = True
+                        self.y += 10
+                        self.drawing(coordinates)
+        
+                if happens.type == pygame.QUIT:
+                    exit()
+                
+
+                if happens.type == pygame.KEYUP:
+                    key = happens.key
+                    if key == pygame.K_LEFT:
+                        left = False
+                    if key == pygame.K_RIGHT:
+                        right = False
+                    if key == pygame.K_UP:
+                        up = False
+                    if key == pygame.K_DOWN:
+                        down = False
+
+        
+                if happens.type == pygame.QUIT:
+                    exit()
+                
+ 
+
+            pygame.display.update()
             pygame.display.flip()
             clock.tick(60)
-           
-
-
-
-
-
-  
   
 
 
 if __name__== "__main__":
     PlatformJumpingGame()
-
 
 
 

55502f022677d49dc524aab6b554c01e68e573b2
Elisa Mero <elisa.mero@helsinki.fi>
2023-04-11 22:27:12 +0300

    debug

--- src/mainplatform.py
+++ src/mainplatform.py
@@ -1,5 +1,3 @@
-#muista git push yms..!
-#import unittest ??!
 
 import os
 import pygame
@@ -15,6 +13,8 @@ class PlatformJumpingGame:
 
         self.player = Player()
 
+        self.jumping = False
+
         self.loop()
 
 
@@ -26,6 +26,7 @@ class PlatformJumpingGame:
             
         
     def background(self):
+        pygame.display.update() 
         pygame.display.set_caption("Jumping Game")
         self.screen.fill((173, 255, 255))
         pygame.draw.circle(self.screen, (255,255,255), (100, 100), 20)
@@ -36,42 +37,42 @@ class PlatformJumpingGame:
 
 
     def draw_player(self):
-        self.player.all_sprites.draw(self.screen)
+        
         self.screen.blit(self.player.user, (self.player.rect.x, self.player.rect.y))
+        self.player.all_sprites.draw(self.screen)
         pygame.display.update() 
   
 
     def moving(self):
+
         clock = pygame.time.Clock()
 
-       
         while True:
+            pygame.display.update() 
+            self.background()
             for happens in pygame.event.get():
                 if happens.type == pygame.QUIT:
                     exit()
-                if happens.type == pygame.KEYDOWN:
-                    key = happens.key
-                    if key == pygame.K_LEFT:
-                        self.player.events("left")
-                        self.draw_player()
-                    if happens.key == pygame.K_RIGHT:
-                        self.player.events("right")
-                        self.draw_player()
-                    if key == pygame.K_UP:
-                        self.player.events("up")
-                        self.draw_player()
-                    if key == pygame.K_DOWN:
-                        self.player.events("down")
-                        self.draw_player()
 
-                
+            usercontrol = pygame.key.get_pressed()
 
-                if happens.type == pygame.KEYUP:
-                    key = happens.key
-                    self.player.events("stop")
+            if usercontrol[pygame.K_RIGHT]:
+                self.player.events("right")
+                self.draw_player()
+            if usercontrol[pygame.K_LEFT]:
+                self.player.events("left")
+                self.draw_player()
+
+            if self.jumping is False and usercontrol[pygame.K_SPACE]:
+                self.jumping = True
+            if self.jumping:
+                self.player.events("jump")
+                if self.player.jump_velocity == 20:
+                    self.jumping = False
      
-            pygame.display.update()    
+            pygame.display.update()  
             pygame.display.flip()
+            pygame.time.delay(20)
             clock.tick(60)
 
 
--- src/objects/playerimage.py
+++ src/objects/playerimage.py
@@ -9,42 +9,36 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-
         self.user = pygame.image.load(
             os.path.join(dirname, "..", "images", "box.png")
         )
 
-        #surface?, also pitääks määrittää et
-        #  tää piirretää self.screenille ??
-
-
         self.rect = self.user.get_rect()
 
-        self.rect.x = 50
-        self.rect.y = 550
+        self.rect.x = 40
+        self.rect.y = 690
         self.all_sprites = pygame.sprite.Group()
+        self.jump_velocity = 20
 
     def sprite(self):
         self.all_sprites.add(self.user)
 
-    def letters(self, h):
-        if h == "x":
-            return self.rect.x
-        if h == "y":
-            return self.rect.y
 
     def events(self, direction):
-        if direction == "right":
-            self.rect.x += 15
-        if direction == "left":
-            self.rect.x -= 15
-        if direction == "up":
-           self.rect.y -= 15
-        if direction == "down":
-            self.rect.y += 15
-        if direction == "stop":
-            self.rect.y == 0
-            self.rect.x == 0
+
+        if direction == "right" and self.rect.x < 800:
+            self.rect.x += 8
+        if direction == "left" and self.rect.x > 0:
+            self.rect.x -= 8
+
+        if direction == "jump":
+            self.rect.y -= self.jump_velocity
+            self.jump_velocity -= 2
+            if self.jump_velocity < -20:
+                self.jump_velocity = 20
+
+        
+
  
 
 
